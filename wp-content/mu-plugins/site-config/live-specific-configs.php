cd417b9d97590e1be23e1fa561b8458107e4eed9
Joe Tower <joe@fourkitchens.com>
2022-02-07 16:33:52 -0600

    simplifying

--- wp-content/mu-plugins/site-config/live-specific-configs.php
+++ wp-content/mu-plugins/site-config/live-specific-configs.php
@@ -1,30 +1,19 @@
 <?php
 
-# List Dev Plugins
-   $dev_plugins = array(
-	'jetpack/jetpack.php',
-	'pantheon-advanced-page-cache.php'
-       );
-
-   $live_plugins = array(
-	'jetpack/jetpack.php',
-	'pantheon-advanced-page-cache.php'
+# List Development Plugins
+   $plugins = array(
+    'jetpack/jetpack.php',
+    'pantheon-advanced-page-cache.php'
        );
 
 # Live-specific configs
    if ( in_array( $_ENV['PANTHEON_ENVIRONMENT'], array( 'live' ) ) ) {
 
-   # Disable Development Plugins
+   # Enable Live Plugins
        require_once(ABSPATH . 'wp-admin/includes/plugin.php');
-       foreach ($dev_plugins as $dev_plugin) {
-           if(is_plugin_active($dev_plugin)) {
-	            deactivate_plugins($dev_plugin);
-           }
-       }
-       # Activate Live Plugins
-       foreach ($live_plugins as $live_plugin) {
-           if(is_plugin_inactive($live_plugin)) {
-               activate_plugin($live_plugin);
+       foreach ($plugins as $plugin) {
+           if(is_plugin_inactive($plugin)) {
+	            activate_plugins($plugin);
            }
        }
 
@@ -34,20 +23,15 @@
 
    # Configs for All environments but Live
    else {
-  	   # Disable Live Plugins
-       require_once(ABSPATH . 'wp-admin/includes/plugin.php');
-       foreach ($live_plugins as $live_plugin) {
-           if(is_plugin_active($live_plugin)) {
-              deactivate_plugins($live_plugin);
-           }
-       }
 
-       # Activate Development Plugins
-       foreach ($dev_plugins as $dev_plugin) {
-           if(is_plugin_inactive($dev_plugin)) {
-               activate_plugin($dev_plugin);
-           }
-       }
+    # Disable Live Plugins
+
+      foreach ($plugins as $plugin) {
+          if(is_plugin_active($plugin)) {
+            deactivate_plugins($plugin);
+          }
+      }
+
    # Enable development mode for jetpack
        add_filter( 'jetpack_development_mode', '__return_true' );
 }
