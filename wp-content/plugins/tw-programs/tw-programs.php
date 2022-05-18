30edf16160077067f19293d92e62d56037d51968
brandonhundt <brandonhundt@gmail.com>
2022-05-18 10:33:54 -0500

    feat(segments): create post type and custom fields

--- wp-content/plugins/tw-programs/tw-programs.php
+++ wp-content/plugins/tw-programs/tw-programs.php
@@ -5,41 +5,50 @@ Description: Creates the Programs custom taxonomy
 */
 
 // Register Custom Taxonomy
-function tw_programs_taxonomy() {
-
-	$labels = array(
-		'name'                       => _x( 'Programs', 'Taxonomy General Name', 'text_domain' ),
-		'singular_name'              => _x( 'Program', 'Taxonomy Singular Name', 'text_domain' ),
-		'menu_name'                  => __( 'Programs', 'text_domain' ),
-		'all_items'                  => __( 'All Programs', 'text_domain' ),
-		'parent_item'                => __( 'Parent Item', 'text_domain' ),
-		'parent_item_colon'          => __( 'Parent Item:', 'text_domain' ),
-		'new_item_name'              => __( 'New Program Name', 'text_domain' ),
-		'add_new_item'               => __( 'Add New Program', 'text_domain' ),
-		'edit_item'                  => __( 'Edit Program', 'text_domain' ),
-		'update_item'                => __( 'Update Program', 'text_domain' ),
-		'view_item'                  => __( 'View Program', 'text_domain' ),
-		'separate_items_with_commas' => __( 'Separate programs with commas', 'text_domain' ),
-		'add_or_remove_items'        => __( 'Add or remove programs', 'text_domain' ),
-		'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
-		'popular_items'              => __( 'Popular Items', 'text_domain' ),
-		'search_items'               => __( 'Search Programs', 'text_domain' ),
-		'not_found'                  => __( 'Not Found', 'text_domain' ),
-		'no_terms'                   => __( 'No items', 'text_domain' ),
-		'items_list'                 => __( 'Items list', 'text_domain' ),
-		'items_list_navigation'      => __( 'Items list navigation', 'text_domain' ),
-	);
-	$args = array(
-		'labels'                     => $labels,
-		'hierarchical'               => false,
-		'public'                     => true,
-		'show_ui'                    => true,
-		'show_admin_column'          => true,
-		'show_in_nav_menus'          => false,
-		'show_tagcloud'              => false,
-		'show_in_rest'               => true,
-	);
-	register_taxonomy( 'program', [ 'post', 'episode' ], $args );
-
+function tw_programs_taxonomy()
+{
+    $labels = [
+        'name' => _x('Programs', 'Taxonomy General Name', 'text_domain'),
+        'singular_name' => _x(
+            'Program',
+            'Taxonomy Singular Name',
+            'text_domain'
+        ),
+        'menu_name' => __('Programs', 'text_domain'),
+        'all_items' => __('All Programs', 'text_domain'),
+        'parent_item' => __('Parent Item', 'text_domain'),
+        'parent_item_colon' => __('Parent Item:', 'text_domain'),
+        'new_item_name' => __('New Program Name', 'text_domain'),
+        'add_new_item' => __('Add New Program', 'text_domain'),
+        'edit_item' => __('Edit Program', 'text_domain'),
+        'update_item' => __('Update Program', 'text_domain'),
+        'view_item' => __('View Program', 'text_domain'),
+        'separate_items_with_commas' => __(
+            'Separate programs with commas',
+            'text_domain'
+        ),
+        'add_or_remove_items' => __('Add or remove programs', 'text_domain'),
+        'choose_from_most_used' => __(
+            'Choose from the most used',
+            'text_domain'
+        ),
+        'popular_items' => __('Popular Items', 'text_domain'),
+        'search_items' => __('Search Programs', 'text_domain'),
+        'not_found' => __('Not Found', 'text_domain'),
+        'no_terms' => __('No items', 'text_domain'),
+        'items_list' => __('Items list', 'text_domain'),
+        'items_list_navigation' => __('Items list navigation', 'text_domain'),
+    ];
+    $args = [
+        'labels' => $labels,
+        'hierarchical' => false,
+        'public' => true,
+        'show_ui' => true,
+        'show_admin_column' => true,
+        'show_in_nav_menus' => false,
+        'show_tagcloud' => false,
+        'show_in_rest' => true,
+    ];
+    register_taxonomy('program', ['post', 'episode', 'segment'], $args);
 }
-add_action( 'init', 'tw_programs_taxonomy', 0 );
+add_action('init', 'tw_programs_taxonomy', 0);
