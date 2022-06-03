85ba99fc1128580c559c04e26c5a5a12ff316b1b
Rick Peterman <rick.peterman@prx.org>
2022-06-03 10:55:34 -0500

    fix(config): mu-plugin load file errors

--- wp-content/mu-plugins/load.php
+++ wp-content/mu-plugins/load.php
@@ -9,15 +9,17 @@
 $wpmu_plugin_dir = opendir( WPMU_PLUGIN_DIR );
 
 // Lists all the entries in this directory.
+$entry = readdir( $wpmu_plugin_dir );
 while ( false !== $entry ) {
-	$entry       = readdir( $wpmu_plugin_dir );
 	$plugin_path = WPMU_PLUGIN_DIR . '/' . $entry;
 
 	// Is the current entry a subdirectory?
-	if ( '.' !== $entry && '...' !== $entry && is_dir( $plugin_path ) ) {
+	if ( '.' !== $entry && '..' !== $entry && is_dir( $plugin_path ) ) {
 		// Includes the corresponding plugin.
-		require $path . '/' . $entry . '.php';
+		require $plugin_path . '/' . $entry . '.php';
 	}
+
+	$entry = readdir( $wpmu_plugin_dir );
 }
 
 // Closes the directory.
