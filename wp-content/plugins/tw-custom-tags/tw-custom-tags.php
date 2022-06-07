9e88f39b985aef99ba184418562b9cde81bb40c3
brandonhundt <brandonhundt@gmail.com>
2022-06-07 12:37:52 -0500

    lint

--- wp-content/plugins/tw-custom-tags/tw-custom-tags.php
+++ wp-content/plugins/tw-custom-tags/tw-custom-tags.php
@@ -1,13 +1,18 @@
 <?php
-/*
-Plugin Name: TW Custom Tags
-Description: Creates the custom taxonomies for metadata
-*/
+/**
+ * Plugin Name: TW Custom Tags
+ * Description: Creates the custom taxonomies for metadata
+ *
+ * @package tw_custom_tags
+ */
 
-require_once('taxonomies/taxonomy-city.php');
-require_once('taxonomies/taxonomy-continent.php');
-require_once('taxonomies/taxonomy-country.php');
-require_once('taxonomies/taxonomy-person.php');
-require_once('taxonomies/taxonomy-province-state.php');
-require_once('taxonomies/taxonomy-region.php');
-require_once('taxonomies/taxonomy-social-tag.php');
+/**
+ * Import Taxonomy files
+ */
+require_once 'taxonomies/taxonomy-city.php';
+require_once 'taxonomies/taxonomy-continent.php';
+require_once 'taxonomies/taxonomy-country.php';
+require_once 'taxonomies/taxonomy-person.php';
+require_once 'taxonomies/taxonomy-province-state.php';
+require_once 'taxonomies/taxonomy-region.php';
+require_once 'taxonomies/taxonomy-social-tag.php';
