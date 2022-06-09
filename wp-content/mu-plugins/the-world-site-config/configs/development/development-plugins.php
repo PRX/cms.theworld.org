95b0ef8313d9648b1f1d7f31bd92d4a5d2dc9363
Sean12mps <sean.michael.piettojo@gmail.com>
2022-06-09 16:11:27 +0700

    Init migration.

--- wp-content/mu-plugins/the-world-site-config/configs/development/development-plugins.php
+++ wp-content/mu-plugins/the-world-site-config/configs/development/development-plugins.php
@@ -5,4 +5,10 @@
  * @package the_world_site_config
  */
 
-define( 'DEVELOPMENT_PLUGINS', array() );
+define(
+	'DEVELOPMENT_PLUGINS',
+	array(
+		'wp-filesystem-ssh2/wp-filesystem-ssh2.php',
+		'fg-drupal-to-wp-premium/fg-drupal-to-wp-premium.php',
+	),
+);
