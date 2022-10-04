ae3da6e87680bd8bc5be91e9c19247ebce31a5f0
brandonhundt <brandonhundt@gmail.com>
2022-10-04 11:08:46 -0500

    vendor files now included

---
+++ wp-content/plugins/newspack-newsletters/vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '93bb45c6d039fd880f9a2e98c1c388d3de4c2c35',
+        'name' => 'automattic/newspack-newsletters',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'automattic/newspack-newsletters' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '93bb45c6d039fd880f9a2e98c1c388d3de4c2c35',
+            'dev_requirement' => false,
+        ),
+        'campaignmonitor/createsend-php' => array(
+            'pretty_version' => 'v6.1.2',
+            'version' => '6.1.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../campaignmonitor/createsend-php',
+            'aliases' => array(),
+            'reference' => '922cec7fbc9da1508c18156db7693a6ddad7194e',
+            'dev_requirement' => false,
+        ),
+        'drewm/mailchimp-api' => array(
+            'pretty_version' => 'v2.5.4',
+            'version' => '2.5.4.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../drewm/mailchimp-api',
+            'aliases' => array(),
+            'reference' => 'c6cdfab4ca6ddbc3b260913470bd0a4a5cb84c7a',
+            'dev_requirement' => false,
+        ),
+    ),
+);
