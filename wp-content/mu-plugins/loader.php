bad22c82b6848475b0008b90e8712e24d7fe7fea
Rick Peterman <rick.peterman@prx.org>
2023-03-01 17:34:32 -0600

    config: gitignore vendor dir
    - remove mu-plugins/load in favor of mu-plugins/loader

--- wp-content/mu-plugins/loader.php
+++ wp-content/mu-plugins/loader.php
@@ -14,6 +14,7 @@ if ( defined( 'WP_INSTALLING' ) && WP_INSTALLING ) {
 // Add mu-plugins here.
 $pantheon_mu_plugins = [
 	'pantheon-mu-plugin/pantheon.php',
+	'the-world-site-config/the-world-site-config.php'
 ];
 
 foreach ( $pantheon_mu_plugins as $file ) {
