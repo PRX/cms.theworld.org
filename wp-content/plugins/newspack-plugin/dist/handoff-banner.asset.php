eae143b9d7a4722de336e74d0ac15311691085bc
Brandon Hundt <brandon.hundt@prx.org>
2023-06-22 15:10:51 -0500

    feat(newspack-plugin): remove

--- wp-content/plugins/newspack-plugin/dist/handoff-banner.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-keycodes', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'f2daf2d77a75cd14fb274c736cfd2726');
