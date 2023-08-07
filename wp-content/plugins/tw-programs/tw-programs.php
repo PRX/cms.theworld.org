11b007cea3ecea37220850ec4d2e765f97f47d01
Rick Peterman <rick.peterman@prx.org>
2023-08-07 14:24:19 -0500

    fix: allow programs in menus
    - enable contributor social links gql api
    - episode temp migration fields not required

--- wp-content/plugins/tw-programs/tw-programs.php
+++ wp-content/plugins/tw-programs/tw-programs.php
@@ -45,7 +45,7 @@ function tw_programs_taxonomy() {
 		'public'              => true,
 		'show_ui'             => true,
 		'show_admin_column'   => true,
-		'show_in_nav_menus'   => false,
+		'show_in_nav_menus'   => true,
 		'show_tagcloud'       => false,
 		'show_in_rest'        => true,
 		'show_in_graphql'     => true,
