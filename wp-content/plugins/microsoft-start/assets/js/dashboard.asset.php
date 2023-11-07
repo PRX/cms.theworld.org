9181b0a94fe8a966dadaf266596f2c531d26975a
Brandon Hundt <brandonhundt@gmail.com>
2023-11-07 09:42:37 -0600

    feat(microsoft-start): upgrade to latest version

--- wp-content/plugins/microsoft-start/assets/js/dashboard.asset.php
+++ wp-content/plugins/microsoft-start/assets/js/dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-i18n'), 'version' => 'e3de935495d6ac742e62');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-i18n'), 'version' => 'd68ab6394820c48c874c');
