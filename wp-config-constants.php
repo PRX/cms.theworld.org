d12a1c563975bb7052eeba48e592ce77a52a92d4
Rick Peterman <rick.peterman@prx.org>
2024-01-18 15:13:25 -0600

    feat(theworld): use frontend preview url's
    - add faust plugin to rewrite non-admin pages to use frontend
    - set auth token cookie frontend can use to get draft content
    - remove unused theme template files

--- wp-config-constants.php
+++ wp-config-constants.php
@@ -1,21 +1,23 @@
 <?php
-/*
- Description: Set up config constants.
- Author: Rick Peterman
-*/
+/**
+ * Description: Set up config constants.
+ * Author: Rick Peterman
+ *
+ * @package tw
+ */
 
 /**
  * Name of server platform. Used to load config files specific to that platform.
  */
-define('SERVER_PLATFORM_NAME', 'pantheon');
+define( 'SERVER_PLATFORM_NAME', 'pantheon' );
 
 /**
  * Name of the environment variable that acts as a flag for the platform server.
  */
-define('SERVER_PLATFORM_ENVIRONMENT_VARIABLE_NAME', 'PANTHEON_ENVIRONMENT');
+define( 'SERVER_PLATFORM_ENVIRONMENT_VARIABLE_NAME', 'PANTHEON_ENVIRONMENT' );
 
 /**
  * Wire up S3 Uploads key and secret values to ENV variable.
  */
-define( 'S3_UPLOADS_KEY', getenv('S3_KEY') );
-define( 'S3_UPLOADS_SECRET', getenv('S3_SECRET') );
+define( 'S3_UPLOADS_KEY', getenv( 'S3_KEY' ) );
+define( 'S3_UPLOADS_SECRET', getenv( 'S3_SECRET' ) );
