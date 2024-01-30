3ac1d2ce38c58f6ed21dfc9f5184897c67752ec7
Rick Peterman <rick.peterman@prx.org>
2024-01-30 12:18:47 -0600

    feat(theworld): more editor config and styles
    - add styles for separator
    - update aligncenter styling to use max-width
    - disable editor welcome popup
    - default to feature image meta box being open
    - remove supports from audio block: align, anchor, spacing

--- wp-content/themes/the-world/js/blockembed.js
+++ wp-content/themes/the-world/js/blockembed.js
@@ -1,4 +1,16 @@
 wp.domReady(function () {
+  //Disable Gutenberg Tour Guide popup.
+  wp.data.select("core/edit-post").isFeatureActive("welcomeGuide") &&
+    wp.data.dispatch("core/edit-post").toggleFeature("welcomeGuide");
+
+  // Make Featured Image opened initially.
+  if (!wp.data.select("core/edit-post").isEditorPanelOpened("featured-image")) {
+    wp.data
+      .dispatch("core/edit-post")
+      .toggleEditorPanelOpened("featured-image");
+  }
+
+  // Allow only certain embed variants.
   const allowedEmbedBlocks = ["twitter", "youtube", "vimeo"];
 
   wp.blocks.getBlockVariations("core/embed").forEach(function (blockVariation) {
@@ -7,9 +19,35 @@ wp.domReady(function () {
     }
   });
 
+  // Remove image style options.
   wp.blocks.unregisterBlockStyle("core/image", ["default", "rounded"]);
 
+  // Remove text format options.
   wp.richText.unregisterFormatType("core/footnote");
   wp.richText.unregisterFormatType("core/text-color");
   wp.richText.unregisterFormatType("core/image");
 });
+
+// Adjust block settings not configurable in `theme.json`.
+wp.hooks.addFilter(
+  "blocks.registerBlockType",
+  "the-world/the-world/log-block-settings",
+  (settings, name) => {
+    switch (name) {
+      case "core/audio":
+        // Disable supports for audio block.
+        return {
+          ...settings,
+          supports: {
+            align: false,
+            anchor: false,
+            spacing: {},
+          },
+        };
+
+      default:
+        return settings;
+    }
+  },
+  100
+);
