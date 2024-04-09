55919d6f2851c5cf6c0213f6ddbfcba7b0f12eaf
Rick Peterman <rick.peterman@prx.org>
2024-04-09 14:29:48 -0500

    config: define S3 upload bucket URL

--- wp-config-production.php
+++ wp-config-production.php
@@ -14,5 +14,7 @@ if ( ! defined( 'WP_DEBUG' ) ) {
 // Configure S3 Uploads.
 define( 'S3_UPLOADS_BUCKET', 'media-pri-org/s3fs-public' );
 define( 'S3_UPLOADS_REGION', 'us-east-1' );
+// Define the base bucket URL (without trailing slash).
+define( 'S3_UPLOADS_BUCKET_URL', 'https://media.pri.org' );
 
 define( 'WP_MEMORY_LIMIT', '512M' );
