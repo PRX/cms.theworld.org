1812c88cee488c61b5e13a82c40601ff2d0acfe7
Rick Peterman <rick.peterman@prx.org>
2024-04-26 15:34:52 -0500

    chore(theworld): remove auth cookie code
    - remove WGraphQL JWT Authorization plugin

--- wp-content/mu-plugins/the-world-site-config/configs/global/global-plugins.php
+++ wp-content/mu-plugins/the-world-site-config/configs/global/global-plugins.php
@@ -52,7 +52,6 @@ define(
 		'svg-block/svg-block.php',
 		'wp-graphql/wp-graphql.php',
 		'wp-graphql-acf/wp-graphql-acf.php',
-		'wp-graphql-jwt-authentication/wp-graphql-jwt-authentication.php',
 		'add-wpgraphql-seo/wp-graphql-yoast-seo.php',
 		'custom-post-type-permalinks/custom-post-type-permalinks.php',
 		'faustwp/faustwp.php',
