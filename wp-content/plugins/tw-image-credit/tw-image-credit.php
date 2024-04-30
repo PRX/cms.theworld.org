6904619d0e6cbe2b845d2bde1a1a7d6851a16d54
Rick Peterman <rick.peterman@prx.org>
2024-04-30 14:30:52 -0500

    feat(twimagecredit): add image credit link
    - add image credit url field
    - update image field groups to display in block editor dialogs
    - add field descriptions to image fields

--- wp-content/plugins/tw-image-credit/tw-image-credit.php
+++ wp-content/plugins/tw-image-credit/tw-image-credit.php
@@ -4,6 +4,8 @@
  * Plugin URI: https://www.dinkuminteractive.com/
  * Description: Add image credit and credit field.
  * Version: 1.0.0
+ *
+ * @package tw-image-credit
  */
 
 /**
@@ -24,14 +26,15 @@ function tw_add_media_credit_to_image_blocks( $block_content, array $block ) {
 	}
 
 	// Get attachment credit using the block id.
-	$attachment_credit = get_post_meta( $block['attrs']['id'], '_media_credit', true );
+	$attachment_credit     = get_post_meta( $block['attrs']['id'], '_media_credit', true );
+	$attachment_credit_url = get_post_meta( $block['attrs']['id'], '_media_credit_url', true );
 	if ( empty( $attachment_credit ) ) {
 		// Not a valid attachment, let's bail.
 		return $block_content;
 	}
 
 	// Filter if needed.
-	$block_content = apply_filters( 'tw_add_media_credit_to_image_blocks', $block_content, $attachment_credit );
+	$block_content = apply_filters( 'tw_add_media_credit_to_image_blocks', $block_content, $attachment_credit, $attachment_credit_url );
 
 	return $block_content;
 }
@@ -42,25 +45,28 @@ add_filter( 'render_block', 'tw_add_media_credit_to_image_blocks', 10, 2 );
  *
  * @param  string $block_content The block content.
  * @param  string $credit        The credit markup.
+ * @param  string $url           The credit url.
  *
  * @return string
  */
-function tw_inject_credit_into_caption( $block_content, $credit ) {
+function tw_inject_credit_into_caption( $block_content, $credit, $url ) {
 
 	// If we have a credit, inject it into the caption markup.
 	if ( $credit ) {
 
 		// Replacement parts.
-		$pattern     = '</figcaption>';
-		$open        = '<span class="media-credit">';
-		$credit = trim( $credit );
-		$close       = '</span>';
+		$pattern = '</figcaption>';
+		$open    = '<span class="media-credit">';
+		$credit  = trim( $credit );
+		$link    = "<a href=\"{$url}\" target=\"_blank\">{$credit}</a>";
+		$content = $url ? $link : $credit;
+		$close   = '</span>';
 
 		// Inject the credit into the caption markup.
-		$block_content = str_replace( $pattern, "{$open}{$credit}{$close}{$pattern}", $block_content );
+		$block_content = str_replace( $pattern, "{$open}{$content}{$close}{$pattern}", $block_content );
 	}
 
 	// Return the modified block content.
 	return $block_content;
 }
-add_filter( 'tw_add_media_credit_to_image_blocks', 'tw_inject_credit_into_caption', 10, 2 );
+add_filter( 'tw_add_media_credit_to_image_blocks', 'tw_inject_credit_into_caption', 10, 3 );
